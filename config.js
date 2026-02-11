accebe660d2b001172ef5eb895fbac203b007438
kanghyunseong <gustjd2754@daum.net>
2026-02-11 14:03:25 +0900

    fix

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const ENV = window.ENV = {
-  //API_URL: "https://examplekang.store",
+  API_URL: "https://examplekang.store",
   CLIENT_URL: "https://examplekang.store",
   VITE_KAKAO_APP_KEY: "539f1788e476505490b4540f73f84a47",
-  API_URL: "http://localhost:8081",
+  // API_URL: "http://localhost:8081",
   /** 라즈베리파이 WebSocket 엔드포인트 (라즈베리파이 IP로 변경) */
   // 실시간 모니터는 "웹소켓 서버가 떠있는 곳"으로 연결해야 합니다.
   // 라즈베리파이가 메트릭을 로컬 백엔드로 POST하고, 로컬 백엔드가 /topic/cpu로 브로드캐스트하는 구조라면
